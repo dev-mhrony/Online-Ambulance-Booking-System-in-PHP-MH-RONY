72ee268533923a31fc87a1fa2f34d9d12d6b26c6
MH RONY <mh.rony.dhanvi@gmail.com>
2024-09-27 11:43:10 +0600

    Last Update - 27-09-2024

--- admin/includes/dbconnection.php
+++ admin/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "ccbd_management");
+$con=mysqli_connect("localhost", "root", "", "ccbd_ambulance");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
