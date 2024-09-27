ee1ecdfebefe3bf6333b3a53513f69ba978b2da7
MH RONY <mh.rony.dhanvi@gmail.com>
2024-09-27 11:32:33 +0600

    Last Update - 27-09-2024

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+			<div class="wthree-copyright">
+			  <p> Emergency Ambulance Portal System Copyright By <a href="https://www.youtube.com/@codecampbdofficial" target="_blank" >Code Camp BD</a></p>
+			</div>
+		  </div>
